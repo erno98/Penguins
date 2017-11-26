8cfebd9c163bdbbf6669492356e60a7271f0e43d
Richard Szczerba <richardszczerba@gmail.com>
2017-11-26 17:48:44 +0100

    Initial Commit

---
+++ Penguins/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Penguins
+//
+//  Created by Richard Szczerba on 26/11/2017.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
